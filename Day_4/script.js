7b37d19b6469acf0712c2ea2378e5f3cdf1ebc2c
rajan1602 <rajanpal2021gaj@gmail.com>
2024-10-15 00:12:52 +0530

    https://github.com/rajan1602/FSWD_5_RAJAN.git

---
+++ Day_4/script.js
@@ -0,0 +1,12 @@
+// function validate(){
+//     let un = document.getElementById("un").value;
+//     let pass = document.getElementById("pass").value;
+
+//     if(un == "admin" && pass == "admin")
+//         document.getElementById("res").innerHTML="Login Success";
+//      else
+//         document.getElementById("res").innerHTML = "Login Failed";
+// }
+let x = 12
+console.log(x);
+let y = 14
